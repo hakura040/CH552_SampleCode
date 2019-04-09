92041575319d62eb0f2e73ff6238982d33e98c96
hakura040 <49109950+hakura040@users.noreply.github.com>
2019-04-09 22:46:07 +0900

    Update USB_Serial.h

--- USB_Serial.h
+++ USB_Serial.h
@@ -1,1 +1,13 @@
-#include <stdio.h>
+#ifndef USB_SERIAL_H_
+#define USB_SERIAL_H_
+
+void USBDeviceCfg();
+void USBDeviceIntCfg();
+void USBDeviceEndPointCfg();
+void EcoSendData(PUINT8 SendBuf);
+void SendData(PUINT8 SendBuf);
+void RecieveData();
+void	USBInterrupt();
+void u16str(UINT16 i_data, char *str_t);
+
+#endif
